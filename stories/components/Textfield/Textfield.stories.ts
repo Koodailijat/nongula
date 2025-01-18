1c7ab6fef2fdc5d2aba8f33a695e55c5aca46b63
Veikko M. <veikkosakari01@gmail.com>
2025-01-18 15:46:32 +0200

    added Textfield component with icon, supporting icon leftside and rightside, with small, medium and big text-size

---
+++ stories/components/Textfield/Textfield.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Textfield } from './Textfield';
+
+const meta = {
+    title: 'Textfield',
+    component: Textfield,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        children: { control: 'text' },
+    },
+} satisfies Meta<typeof Textfield>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Medium_and_Right: Story = {
+    args: {
+        children: 'Search',
+        size: 'medium',
+        icon_side: "right"
+    },
+};
+
+export const Medium_and_Left: Story = {
+    args: {
+        children: 'Search',
+        size: 'medium',
+    },
+};
+
+export const Large: Story = {
+    args: {
+        children: 'Search',
+        size: 'large',
+    },
+};
---
+++ stories/components/Textfield/Textfield.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export interface TextfieldProps {
+    /** Text to show **/
+    children: string;
+    /** Text color, defaults to primary **/
+    mode?: 'primary' | 'secondary';
+    /** Text size, defaults to medium **/
+    size?: 'small' | 'medium' | 'large';
+    /** Side of the icon **/
+    icon_side?: "left" | "right";
+    /** URL of the icon image **/
+    icon?: string;
+}
+
+/** Primary UI component for text */
+export const Textfield = ({
+    children,
+    size = 'medium',
+    mode = 'primary',
+    icon_side = 'left',
+    icon = "../../../search_icon_textfield.PNG",
+    ...props
+}: TextfieldProps) => {
+    return (
+        <div className="storybook-textfield-wrapper" 
+        style={{
+            display: "flex", 
+            flexDirection: "row", 
+            gap: "10px", 
+            outline: "1px solid black",
+            borderRadius: "15px",
+            padding: "10px",
+            justifyContent: icon_side === "right" ? "flex-end" : "flex-start",
+            width: "200px",
+            alignItems: "center"
+        }}>
+            {icon_side === "left" && icon && (
+                <div className="storybook-textfield-icon">
+                    <img src={icon} alt="Icon" className="storybook-textfield-icon" style={{width: "18px"}}/>
+                </div>
+            )}
+            <div className="storybook-textfield-content">
+                <span
+                    className={['text', `text--${size}`, `text--${mode}`].join(' ')}
+                    {...props}>
+                    {children}
+                </span>
+            </div>
+            {icon_side === "right" && icon && (
+                <div className="storybook-textfield-icon">
+                    <img src={icon} alt="Icon" className="storybook-textfield-icon" style={{width: "20px"}}/>
+                </div>
+            )}
+        </div>
+    );
+};
