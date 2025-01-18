a1e937b57695dc3a316b30bef154ada17b69b517
Veikko M. <veikkosakari01@gmail.com>
2025-01-18 20:18:28 +0200

    prettier

--- stories/components/Textfield/Textfield.tsx
+++ stories/components/Textfield/Textfield.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export interface TextfieldProps {
     /** Text to show **/
@@ -8,7 +8,7 @@ export interface TextfieldProps {
     /** Text size, defaults to medium **/
     size?: 'small' | 'medium' | 'large';
     /** Side of the icon **/
-    icon_side?: "left" | "right";
+    icon_side?: 'left' | 'right';
     /** URL of the icon image **/
     icon?: string;
 }
@@ -19,37 +19,55 @@ export const Textfield = ({
     size = 'medium',
     mode = 'primary',
     icon_side = 'left',
-    icon = "../../../search_icon_textfield.PNG",
+    icon = '../../../search_icon_textfield.PNG',
     ...props
 }: TextfieldProps) => {
     return (
-        <div className="storybook-textfield-wrapper" 
-        style={{
-            display: "flex", 
-            flexDirection: "row", 
-            gap: "10px", 
-            outline: "1px solid black",
-            borderRadius: "15px",
-            padding: "10px",
-            justifyContent: icon_side === "right" ? "flex-end" : "flex-start",
-            width: "200px",
-            alignItems: "center"
-        }}>
-            {icon_side === "left" && icon && (
-                <div className="storybook-textfield-icon" style={{display: "flex", alignItems: "center"}}>
-                    <img src={icon} alt="Icon" className="storybook-textfield-icon" style={{width: "20px"}}/>
+        <div
+            className="storybook-textfield-wrapper"
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '10px',
+                outline: '1px solid black',
+                borderRadius: '15px',
+                padding: '10px',
+                justifyContent:
+                    icon_side === 'right' ? 'flex-end' : 'flex-start',
+                width: '200px',
+                alignItems: 'center',
+            }}>
+            {icon_side === 'left' && icon && (
+                <div
+                    className="storybook-textfield-icon"
+                    style={{ display: 'flex', alignItems: 'center' }}>
+                    <img
+                        src={icon}
+                        alt="Icon"
+                        className="storybook-textfield-icon"
+                        style={{ width: '20px' }}
+                    />
                 </div>
             )}
             <div className="storybook-textfield-content">
                 <span
-                    className={['text', `text--${size}`, `text--${mode}`].join(' ')}
+                    className={['text', `text--${size}`, `text--${mode}`].join(
+                        ' '
+                    )}
                     {...props}>
                     {children}
                 </span>
             </div>
-            {icon_side === "right" && icon && (
-                <div className="storybook-textfield-icon" style={{display: "flex", alignItems: "center"}}>
-                    <img src={icon} alt="Icon" className="storybook-textfield-icon" style={{width: "20px", alignItems: "center"}}/>
+            {icon_side === 'right' && icon && (
+                <div
+                    className="storybook-textfield-icon"
+                    style={{ display: 'flex', alignItems: 'center' }}>
+                    <img
+                        src={icon}
+                        alt="Icon"
+                        className="storybook-textfield-icon"
+                        style={{ width: '20px', alignItems: 'center' }}
+                    />
                 </div>
             )}
         </div>
