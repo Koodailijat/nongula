6b6a89fcaa22f3f29a13ebfdbf44d9397031b4b0
Joonatan Kallio <kallio.joonatan1@gmail.com>
2025-01-23 20:17:14 +0200

    6 some fixes and react 19 now in use

--- stories/components/Button/Button.stories.tsx
+++ stories/components/Button/Button.stories.tsx
@@ -1,22 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-
 import { Button } from './Button';
+import { CirclePlus, User } from 'lucide-react';
 
 const meta = {
-    title: 'Example/Button',
+    title: 'Button',
     component: Button,
     parameters: {
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: 'centered',
     },
     tags: ['autodocs'],
-
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -24,32 +16,29 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
     args: {
-        primary: true,
         label: 'Button',
+        icon: <CirclePlus color="white" />,
     },
 };
 
 export const Medium: Story = {
     args: {
-        primary: true,
         label: 'Button',
+        icon: <User color="white" />,
     },
 };
 
 export const Large_and_outlined: Story = {
     args: {
-        primary: true,
         size: 'large',
         label: 'Button',
-        style: 'outlined',
+        outline: true,
     },
 };
 
 export const Small: Story = {
     args: {
-        primary: true,
         size: 'small',
         label: 'Button',
-        style: 'none ',
     },
 };
--- stories/components/Button/Button.tsx
+++ stories/components/Button/Button.tsx
@@ -1,54 +1,40 @@
+import React from 'react';
+
 export interface ButtonProps {
-    /** Is this the principal call to action on the page? */
-    primary?: boolean;
-    /** What background color to use */
-    backgroundColor?: string;
-    /** How large should the button be? */
+    /** Button size */
     size?: 'small' | 'medium' | 'large';
-    /** Button contents */
+    /** Button Text */
     label: string;
-    /** Button style */
-    style?: 'outlined' | 'none';
+    /** Button outline */
+    outline?: boolean;
     /** Optional click handler */
     onClick?: () => void;
+    /** Icon as react component */
+    icon?: React.ReactNode;
 }
 
 /** Primary UI component for user interaction */
 export const Button = ({
-    primary = false,
     size = 'medium',
-    backgroundColor,
     label,
-    style = 'none',
+    outline = false,
+    icon,
     ...props
 }: ButtonProps) => {
-    const mode = primary ? 'button--primary' : 'button--secondary';
     return (
         <div
             className={[
                 'button-wrapper',
-                `button-wrapper--${style}`,
-                `button--${style}`,
+                `${outline ? 'button-wrapper--outline' : ''}`,
             ].join(' ')}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-circle-plus">
-                <circle cx="12" cy="12" r="10" />
-                <path d="M8 12h8" />
-                <path d="M12 8v8" />
-            </svg>
+            {icon}
             <button
                 type="button"
-                className={['button', `button--${size}`, mode].join(' ')}
-                style={{ backgroundColor }}
+                className={[
+                    'button',
+                    `button--${size}`,
+                    'button--primary',
+                ].join(' ')}
                 {...props}>
                 {label}
             </button>
