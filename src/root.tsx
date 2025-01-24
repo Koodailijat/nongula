c9c421a7f1c9d9a0a4db000e395c3bc56d3699bd
Joonatan Kallio <80262292+JoonatanKallio@users.noreply.github.com>
2025-01-25 00:59:47 +0200

    30 added router and two routes with error boundary route (#34)
    * 30 added router and two routes with error boundary route
    
    * 30 PR changes

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,18 @@
-import { createRoot } from 'react-dom/client';
-import { Main } from './main.tsx';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { ErrorRoute } from './routes/errorroute/errorroute.tsx';
+import { ModifyRoute } from './routes/modifyroute/modifyroute.tsx';
+import { DashboardRoute } from './routes/dashboardroute/dashboardroute.tsx';
+import '../stories/_nongula.scss';
 
-createRoot(document.getElementById('root')!).render(<Main />);
+const root = document.getElementById('root')!;
+
+ReactDOM.createRoot(root).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path={'*'} element={<ErrorRoute />} />
+            <Route path={'/'} element={<DashboardRoute />} />
+            <Route path={'/modify'} element={<ModifyRoute />} />
+        </Routes>
+    </BrowserRouter>
+);
