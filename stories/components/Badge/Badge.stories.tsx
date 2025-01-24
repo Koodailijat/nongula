aeab2607effd4de76cd72e4f8d04976f25c36dce
Veikko M. <veikkosakari01@gmail.com>
2025-01-24 23:04:32 +0200

    removed stories references to sizes

--- stories/components/Badge/Badge.stories.tsx
+++ stories/components/Badge/Badge.stories.tsx
@@ -15,14 +15,12 @@ type Story = StoryObj<typeof meta>;
 
 export const Small: Story = {
     args: {
-        size: 'small',
         children: '5',
     },
 };
 
 export const Medium: Story = {
     args: {
-        size: 'medium',
         children: '5',
     },
 };
