d2830aa5ae7fba2c111e4b2c9618ff36de28a960
Veikko M. <veikkosakari01@gmail.com>
2025-01-24 22:43:14 +0200

    removed unnecessary classes and props

--- stories/components/Badge/Badge.tsx
+++ stories/components/Badge/Badge.tsx
@@ -1,17 +1,13 @@
 import { ComponentProps } from 'react';
 
 type BadgeProps = ComponentProps<'span'> & {
-    /** Text color **/
-    size?: 'small' | 'medium';
     children?: React.ReactNode;
 };
 /** Primary UI component for badge */
-export const Badge = ({ children, size = 'medium', ...props }: BadgeProps) => {
+export const Badge = ({ children, ...props }: BadgeProps) => {
     return (
         <div className={'badge-wrapper'}>
-            <span className={[`badge-icon--${size}`].join(' ')} {...props}>
-                {children}
-            </span>
+            <span {...props}>{children}</span>
         </div>
     );
 };
