14d53c40abc1a84071793a355fb04ed63f1960ca
Topi Ilmoni <topi.ilmoni@gmail.com>
2025-01-25 01:00:44 +0200

    Created List and ListItem components

---
+++ stories/components/List/List.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { List } from './List.tsx';
+import { Text } from '../Text/Text.tsx';
+import { ListItem } from './ListItem.tsx';
+import { IconButton } from '../IconButton/IconButton.tsx';
+import { Pen, Trash } from 'lucide-react';
+import { useListData } from 'react-stately';
+
+const meta: Meta<typeof List> = {
+    title: 'List',
+    component: List,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    render: () => {
+        const initialItems = [
+            { id: 1, text: '321 calories' },
+            { id: 2, text: '1502 calories' },
+            { id: 3, text: '1259 calories' },
+            { id: 4, text: '2565 calories' },
+            { id: 5, text: '753 calories' },
+        ];
+
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const list = useListData({
+            initialItems,
+        });
+
+        return (
+            <List items={list.items}>
+                {(item) => (
+                    <ListItem key={item.id} id={item.id}>
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                                gap: '16px',
+                            }}>
+                            <Text>{item.text}</Text>
+                            <div style={{ display: 'flex' }}>
+                                <IconButton icon={<Pen strokeWidth={3} />} />
+                                <IconButton
+                                    icon={<Trash strokeWidth={3} color="red" />}
+                                    onPress={() => {
+                                        list.remove(item.id);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </ListItem>
+                )}
+            </List>
+        );
+    },
+};
