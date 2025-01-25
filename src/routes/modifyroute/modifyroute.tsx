7c3b21b2728608a9ad281d6d82204c0eebdd8550
Veikko M. <veikkosakari01@gmail.com>
2025-01-25 13:34:33 +0200

    added 3 components to modify calories layout, list and textfield missing

--- src/routes/modifyroute/modifyroute.tsx
+++ src/routes/modifyroute/modifyroute.tsx
@@ -1,3 +1,24 @@
+import { Heading } from '../../../stories/components/Heading/Heading.tsx';
+import { ProgressBar } from '../../../stories/components/ProgressBar/ProgressBar.tsx';
+import { Button } from '../../../stories/components/Button/Button.tsx';
+import { CirclePlus } from 'lucide-react';
+
 export function ModifyRoute() {
-    return <div>Modify calories here!</div>;
+    return (
+        <div className={'modify'}>
+            <div className={'modify-wrapper'}>
+                <Heading level={4}>Tuesday 21st</Heading>
+                <ProgressBar
+                    style={{ width: '250px' }}
+                    label={"Today's calories"}
+                    value={30}
+                    valueText={'2000 kcal'}
+                />
+                <Button
+                    icon={<CirclePlus color="white" />}
+                    children={'Custom calories'}
+                />
+            </div>
+        </div>
+    );
 }
