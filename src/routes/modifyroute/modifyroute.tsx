c73f883f3a58c50563e89541c4e09201dca154a9
Veikko M. <80270045+IT-miez@users.noreply.github.com>
2025-01-25 17:44:39 +0200

    33 create modify calories layout with components (#41)
    * added 3 components to modify calories layout, list and textfield missing
    
    * added textfield
    
    * added modal in modifyroute
    
    * added modal with modify-screen
    
    * saved list initialization
    
    * styled progress bar
    
    * prettier
    
    * 33 removed inline styling
    
    * 33 removed more inline styling
    
    ---------
    
    Co-authored-by: Joonatan Kallio <kallio.joonatan1@gmail.com>

--- src/routes/modifyroute/modifyroute.tsx
+++ src/routes/modifyroute/modifyroute.tsx
@@ -1,15 +1,68 @@
-import { useState } from 'react';
+import { Heading } from '../../../stories/components/Heading/Heading.tsx';
+import { ProgressBar } from '../../../stories/components/ProgressBar/ProgressBar.tsx';
 import { Button } from '../../../stories/components/Button/Button.tsx';
 import { CustomCaloriesModal } from './components/customcaloriesmodal.tsx';
-import { PlusIcon } from 'lucide-react';
+import { Pen, PlusIcon, SearchIcon, Trash } from 'lucide-react';
 import './modifyroute.scss';
+import { useState } from 'react';
+import { useListData } from 'react-stately';
+import { TextField } from '../../../stories/components/TextField/TextField.tsx';
+import { List } from '../../../stories/components/List/List.tsx';
+import { ListItem } from '../../../stories/components/List/ListItem.tsx';
+import { IconButton } from '../../../stories/components/IconButton/IconButton.tsx';
+import { Text } from '../../../stories/components/Text/Text.tsx';
 
 export function ModifyRoute() {
     const [isOpen, setOpen] = useState(false);
 
+    // Mock data
+    const initialItems = [
+        { id: 1, text: '300 calories' },
+        { id: 2, text: '300 calories' },
+        { id: 3, text: '300 calories' },
+        { id: 4, text: '300 calories' },
+        { id: 5, text: '300 calories' },
+    ];
+
+    const list = useListData({
+        initialItems,
+    });
+
     return (
         <div className="modify-route">
+            <Heading level={4}>Tuesday 21st</Heading>
             <div className="modify-route__content">
+                <div className="modify-route__progress-bar">
+                    <ProgressBar
+                        label={"Today's calories"}
+                        value={30}
+                        valueText={'2000 kcal'}
+                    />
+                </div>
+                <TextField
+                    iconSide={'left'}
+                    icon={<SearchIcon />}
+                    placeholder={'Search'}
+                />
+                <List className="modify-route__list" items={list.items}>
+                    {({ id, text }) => (
+                        <ListItem
+                            className="modify-route__list-item"
+                            key={id}
+                            id={id}>
+                            <Text>{text}</Text>
+                            <div className="modify-route__list-actions">
+                                <IconButton icon={<Pen strokeWidth={2} />} />
+                                <IconButton
+                                    icon={<Trash strokeWidth={2} color="red" />}
+                                    onPress={() => {
+                                        list.remove(id);
+                                    }}
+                                />
+                            </div>
+                        </ListItem>
+                    )}
+                </List>
                 <Button
                     onPress={() => setOpen(true)}
                     size="large"
