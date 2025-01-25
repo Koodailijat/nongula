ec8dcce17ada52c967354dc599167b8c9dbbda75
Veikko M. <80270045+IT-miez@users.noreply.github.com>
2025-01-25 16:05:06 +0200

    31 create custom calories modal (#36)
    * added modal route with components and scss-file
    
    * added segmented control to show the other modal, modified textfield label font size
    
    * edited modal scss
    
    * Now uses Modal component
    
    * added gap, added heading to modal
    
    * prettier fix
    
    * renamed placeholders, fixedcentering of elements
    
    * added isRequired to textfield
    
    ---------
    
    Co-authored-by: Topi Ilmoni <topi.ilmoni@gmail.com>
    Co-authored-by: Joonatan Kallio <kallio.joonatan1@gmail.com>

--- stories/components/TextField/TextField.tsx
+++ stories/components/TextField/TextField.tsx
@@ -35,7 +35,7 @@ export const TextField = ({
 }: TextFieldProps) => {
     return (
         <RATextfield className="textfield" {...props}>
-            {label && <RALabel>{label}</RALabel>}
+            {label && <RALabel className={'RALabel'}>{label}</RALabel>}
             <div className="input-container">
                 {iconSide === 'left' && icon && (
                     <div className="textfield-icon textfield-icon--left">
